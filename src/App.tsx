e179292ccd795701bcd642708890d42020656c4b
matveev96 <sasha.matveev1996@icloud.com>
2024-12-24 17:12:16 +0300

    Change useState on useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Todolist} from "./Todolist";
+import {useReducer} from "react";
+import {v1} from "uuid";
+import {addTaskAC, removeTaskAC, tasksReducer} from "./model/tasks-reducer";
+import {changeFilterAC, filterReducer} from "./model/filter-reducer";
+
+export type TaskType = {
+	id: string
+	title: string
+	isDone: boolean
+}
+
+export type FilterValuesType = 'all' | 'active' | 'completed'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [tasks, dispatchTasks] = useReducer(tasksReducer,[
+		{id: v1(), title: 'HTML&CSS', isDone: true},
+		{id: v1(), title: 'JS', isDone: true},
+		{id: v1(), title: 'ReactJS', isDone: false},
+	])
+
+	const [filter, dispatchFilter] = useReducer(filterReducer, 'all')
+
+	const removeTask = (taskId: string) => {
+		dispatchTasks(removeTaskAC(taskId))
+	}
+
+	const addTask = (title: string)=> {
+		dispatchTasks(addTaskAC(title))
+	}
+
+	const changeFilter = (filter: FilterValuesType) => {
+		dispatchFilter(changeFilterAC(filter))
+	}
+
+	let tasksForTodolist = tasks
+	if (filter === 'active') {
+		tasksForTodolist = tasks.filter(task => !task.isDone)
+	}
+
+	if (filter === 'completed') {
+		tasksForTodolist = tasks.filter(task => task.isDone)
+	}
+
+	return (
+		<div className="App">
+			<Todolist
+				title="What to learn"
+				tasks={tasksForTodolist}
+				removeTask={removeTask}
+				changeFilter={changeFilter}
+				addTask={addTask}
+			/>
+		</div>
+	);
 }
 
 export default App;
